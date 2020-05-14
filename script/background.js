0fda933acd79628d45285c2a15679f6b8f4ebd97
fabio1999ita <fabio1999ita@gmail.com>
2020-05-14 17:07:24 +0200

    add lang

--- script/background.js
+++ script/background.js
@@ -2,7 +2,7 @@
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
 	chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
 	if (message.db != true) {
-		chrome.tabs.create({url:"https://" + message.sv + ".darkorbit.com/indexInternal.es?action=internalStart"},function(newTab) {
+		chrome.tabs.create({url:"https://" + message.sv + ".darkorbit.com/indexInternal.es?action=internalStart" + message.lg},function(newTab) {
 		});
 	}
 });
