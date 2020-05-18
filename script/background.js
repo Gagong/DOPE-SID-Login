f04cbc6d8d2f8c3b039140c471ee53e79cef0cfb
fabio1999ita <fabio1999ita@gmail.com>
2020-05-18 19:50:28 +0200

    add incognito mode

--- script/background.js
+++ script/background.js
@@ -1,8 +1,14 @@
 //Credits: Popcorn
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
-	chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
-	if (message.db != true) {
-		chrome.tabs.create({url:"https://" + message.sv + ".darkorbit.com/indexInternal.es?action=internalStart" + message.lg},function(newTab) {
+	if(message.incognito != true)
+		chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
+
+	if (message.db != true && message.incognito != true) 
+		chrome.tabs.create({url:"https://" + message.sv + ".darkorbit.com/indexInternal.es?action=internalStart" + message.lg});
+
+	if (message.db != true && message.incognito == true) {
+		chrome.windows.create({"url": "", "incognito": true},function(newWin) {
+			chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
 		});
 	}
 });
