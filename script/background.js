5912d12c2718c79ebf04d080e27ec5f2129f2f92
fabio1999ita <fabio1999ita@gmail.com>
2020-05-21 18:47:16 +0200

    fast login

--- script/background.js
+++ script/background.js
@@ -1,14 +1,13 @@
-//Credits: Popcorn
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
 	if(message.incognito != true)
 		chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
 
 	if (message.db != true && message.incognito != true) 
-		chrome.tabs.create({url:"https://" + message.sv + ".darkorbit.com/indexInternal.es?action=internalStart" + message.lg});
+		chrome.tabs.create({url:"https://" + message.sv + message.url + message.lg});
 
 	if (message.db != true && message.incognito == true) {
-		chrome.windows.create({"url": "", "incognito": true},function(newWin) {
-			chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
+		chrome.windows.create({"url": "https://" + message.sv + ".darkorbit.com/?dosid=" + message.sid, "incognito": true},function(newWin) {
+
 		});
 	}
 });
