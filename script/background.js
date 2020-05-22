1720cae48bc644f7751ccf936d9a93d1caa4e9b2
fabio1999ita <fabio1999ita@gmail.com>
2020-05-22 15:25:02 +0200

    add auto enable flash

--- script/background.js
+++ script/background.js
@@ -1,4 +1,16 @@
 chrome.extension.onMessage.addListener(function (message, sender, callback) {
+	var flashResIdentifier; 
+   	chrome.contentSettings.plugins.getResourceIdentifiers(function(resIdentifiers) {
+   		for (var i=0; i<resIdentifiers.length; i++) {
+			console.log(resIdentifiers[i]);
+   			if (resIdentifiers[i].id == "adobe-flash-player") {
+   				flashResIdentifier = resIdentifiers;
+   				break;
+   			}
+   		}
+   	});
+   	chrome.contentSettings.plugins.set({'primaryPattern':"https://*.darkorbit.com/*", 'resourceIdentifier':flashResIdentifier, 'setting':'allow'});
+
 	if(message.incognito != true)
 		chrome.cookies.set({name:"dosid",url:"https://" + message.sv + ".darkorbit.com", value: message.sid});
 
